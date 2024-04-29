9b41da4a14e7879fc5c1e424a489d1631ce01d1c
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-04-29 09:32:03 +0500

    add new

---
+++ src/BookStore.Application/useCases/Users/Queries/GetAllNewUsersQuery.cs
@@ -0,0 +1,10 @@
+﻿using BookStore.Domain.Entities;
+using BookStore.Domain.Entities.Auth;
+using MediatR;
+
+namespace BookStore.Application.useCases.Users.Queries
+{
+    public class GetAllNewUsersQuery : IRequest<List<User>>
+    {
+    }
+}
--- src/BookStore.Presentation/MVC/Controllers/AdminController.cs
+++ src/BookStore.Presentation/MVC/Controllers/AdminController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Domain.Entities.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
 
 namespace MVC.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly UserManager<User> _userManager;
+
+        public AdminController(UserManager<User> userManger)
+        {
+            _userManager = userManger;
+        }
+
         public IActionResult Index()
         {
+
             return View();
         }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Profile()
+        {
+            
+        }
+
     }
 }
